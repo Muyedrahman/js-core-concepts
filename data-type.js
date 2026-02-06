fe6a38f7b632100e09c69de5c6ae1dcdd6811842
Muyedur rahman <muyedrahman732@gmail.com>
2026-02-06 14:20:43 +0600

    data type

---
+++ data-type.js
@@ -0,0 +1,14 @@
+let data = 4;
+data = 'tormuj';
+data =false;
+console.log(typeof data);
+
+let x = 5 + "5";
+console.log(x);
+console.log(typeof x);
+
+
+// let datas = 10;
+// data = "10";
+// data = true;
+// console.log(typeof data);
