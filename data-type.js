9fdd6305ba4f7091e862c159d24029669ee070ef
Muyedur rahman <muyedrahman732@gmail.com>
2026-02-07 22:07:37 +0600

    add primtive and non primitive data type

--- data-type.js
+++ data-type.js
@@ -1,29 +1,46 @@
+// JS is a dynamic typed programming language
 let data = 4;
 data = 'tormuj';
 data =false;
-console.log(typeof data);
+data = {name: 'vaangi', price:40}
+data = [ 445 ]
+data = undefined
+data = null
+console.log(typeof data); // m
+// js: primitive data type
+const price = 50;
+const name = ' bodrul'
+const isPoor = false;
+let isKnown = undefined;
+let isLocal;
+const merit = null;
+console.log(isKnown, isLocal)
 
-let x = 5 + "5";
-console.log(x);
-console.log(typeof x);
+// non-primitive data type
+const student = {name: 'badal', location: 'bristi goli', subject: 'botany'}
+const friends = ['tom', 'bob', 'jack', 'rock']
+
+// let x = 5 + "5";
+// console.log(x);
+// console.log(typeof x);
 
 
-let datas = 50;
-datas = "50";
-datas = true;
-console.log(typeof datas);
+// let datas = 50;
+// datas = "50";
+// datas = true;
+// console.log(typeof datas);
 
 
-console.log("10" * "2");
-console.log(typeof ("10" * "2"));
+// console.log("10" * "2");
+// console.log(typeof ("10" * "2"));
 
 
-console.log("5" + 2);
-console.log("5" - 2);
+// console.log("5" + 2);
+// console.log("5" - 2);
 
-console.log("5" + 2);
-console.log("5" - 2);
+// console.log("5" + 2);
+// console.log("5" - 2);
 
-console.log([] + []);
-console.log(typeof ([] + []));
+// console.log([] + []);
+// console.log(typeof ([] + []));
 
