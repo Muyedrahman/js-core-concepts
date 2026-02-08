4652aac8c0ac1405544d909523e37de4a819b677
Muyedur rahman <muyedrahman732@gmail.com>
2026-02-08 21:51:34 +0600

    Practice truthy and falsy concept in JS

---
+++ double-equal.js
@@ -0,0 +1,10 @@
+console.log(2 == 2);
+console.log(2 == '2');
+console.log(2 == '1');
+// console.log(2 === "2");
+// console.log(1 === 2);
+console.log(true == 1);  //1 == '1' --> 1 == 1 
+console.log(false == 0);
+// console.log(null === undefined);
+// console.log(null === undefined);
+// console.log(0 === "");
